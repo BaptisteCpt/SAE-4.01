ee98f9160bb050ae43c979baeb555e52e4874979
Noa Doisy <noa.doisy@etu.univ-grenoble-alpes.fr>
2025-11-18 08:13:38 +0000

    Documentation du code de la page d'accueil

--- sae3.01/app/api/login/route.js
+++ sae3.01/app/api/login/route.js
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../lib/prisma';
 
-export async function POST(request) {
-  const { login, mot_de_passe } = await request.json();
-  const user = await prisma.user.findUnique({ where: { login } }); /* requete pour récup un utilisateur basé sur son login unique*/
+/**
+ * Fonction permettant de verifier si l'identifiant et le mot de passe saisie sont bons
+ */
+export async function POST(request) { /*Creation de la fonction POST prenant en parametre une requette*/
+  const { login, mot_de_passe } = await request.json(); /*Recuperation des données du body de la requette HTTP puis decoupage en 2 variables*/
+  const user = await prisma.user.findUnique({ where: { login } }); /**Requete qui tente de trouver un login correspondant a la requete dans la table USER */
 
-  if (!user || user.mot_de_passe !== mot_de_passe) {
-    return NextResponse.json({ error: 'Identifiants invalides' }, { status: 401 });
+  if (!user || user.mot_de_passe !== mot_de_passe) { /*Verification : si l'utilisateur n'a pas été trouvé ou que le mot de passe de cet utilisateur est different de celui saisie*/
+    return NextResponse.json({ error: 'Identifiants invalides' }, { status: 401 });/*Renvoi une reponse JSON Erreur si la condition est verifie*/
   }
 
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true }); /* Renvoi d'une reponse JSON de Succes*/
 }
