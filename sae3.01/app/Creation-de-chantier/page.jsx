0a60c01455216e503ee327aef309b0d2de962149
tajeri <ilyess.tajer@etu.univ-grenoble-alpes.fr>
2025-11-18 10:16:35 +0100

    Ajout des formulaires

--- sae3.01/app/Creation-de-chantier/page.jsx
+++ sae3.01/app/Creation-de-chantier/page.jsx
@@ -1,13 +1,18 @@
 'use client'
 
 import React from 'react'
+import {useState} from 'react'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import Nav from '../components/Nav'
+import '../css/creation_chantier.css'
 
 export default function page() {
   return (
     <>
         {
             <div>
+                <Nav/>
                 <h1>Création d'un Chantier</h1>
                 <form>
                     <label>
@@ -16,27 +21,27 @@ export default function page() {
                     <br />
                     <label>
                         Nom:
-                        <input type="text" name="Nom..." />
+                        <input type="text" name="Nom" />
                     </label>
                     <br />
                     <label>
                         Prénom:
-                        <input type="text" name="Prénom..." />
+                        <input type="text" name="Prenom" />
                     </label>
                     <br />
                     <label>
                         Adresse:
-                        <input type="text" name="Adresse..." />
+                        <input type="text" name="Adresse" />
                     </label>
                     <br />
                     <label>
                         Ville:
-                        <input type="text" name="Ville..." />
+                        <input type="text" name="Ville" />
                     </label>
                     <br />
                     <label>
                         Code Postal:
-                        <input type="text" name="Code Postal..." />
+                        <input type="text" name="CodePostal" />
                     </label>
                     <br />
                     <button type="submit">Continuer</button>
