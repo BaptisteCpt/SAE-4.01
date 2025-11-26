055630f11ca4e3607a7757705ea0b091e920fcfd
scanue <esteban.scanu@outlook.fr>
2025-11-26 19:11:21 +0100

    fin nav

--- sae3.01/app/acceuil/page.jsx
+++ sae3.01/app/acceuil/page.jsx
@@ -5,22 +5,26 @@ import Nav_commercial from '../components/Nav_commercial'
 import Nav_admin from '../components/Nav_admin'
 import Nav_maitreO from '../components/Nav_maitreO'
 import styles from '../css/accueil.css'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
   const role = localStorage.getItem("role");
   const nom = localStorage.getItem("nom");
+  const router = useRouter();
+
   let nav = null;
 
   if(role == "admin") {
     nav = <Nav_admin />
   }
-
-  if(role == "commercial") {
+  else if(role == "commercial") {
     nav = <Nav_commercial/>
   }
-
-  if(role == "maitre Oeuvre") {
+  else if(role == "maitre Oeuvre") {
     nav = <Nav_maitreO/>
+  }
+  else {
+    router.push('/');
   }
 
   return (
