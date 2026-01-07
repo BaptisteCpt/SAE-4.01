28e73381bbc33624c6a41d0af17793ba33e3c877
frilo <ilyesstajer8@gmail.com>
2026-01-07 13:31:23 +0100

    ajout api bis

---
+++ sae3.01/app/api/recup_arti_bis/route.js
@@ -0,0 +1,13 @@
+export async function GET() {
+  try {
+    const artisans = await prisma.artisan.findMany({
+      orderBy: { noartisan: 'asc' },
+      include: {
+        etre_qualifie_pour: true 
+      }
+    });
+    return NextResponse.json(artisans);
+  } catch (err) {
+    return NextResponse.json({ error: "Erreur serveur" });
+  }
+}
