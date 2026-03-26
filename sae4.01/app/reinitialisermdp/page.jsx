6c444d0e5cef520f9a44449c177daecdab9ae15a
BaptisteCpt <coupatbaptiste7@gmail.com>
2026-03-26 12:19:44 +0100

    Ajout d'un suspense

--- sae4.01/app/reinitialisermdp/page.jsx
+++ sae4.01/app/reinitialisermdp/page.jsx
@@ -1,15 +1,17 @@
-'use client'
+"use client";
 
 import Reset from "../components/ResetPassWord";
 import "../css/profil.css";
 
 export default function ReinitialiserMdp() {
-    return (
-        <main className="profil-page">
-            <div className="bulle_login">
-                <h1>Réinitialiser votre mot de passe</h1>
-                <Reset />
-            </div>
-        </main>
-    );
+  return (
+    <main className="profil-page">
+      <div className="bulle_login">
+        <h1>Réinitialiser votre mot de passe</h1>
+        <Suspense fallback={<p>Chargement du formulaire...</p>}>
+          <Reset />
+        </Suspense>{" "}
+      </div>
+    </main>
+  );
 }
